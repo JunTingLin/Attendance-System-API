df007e9178dec4ba7abd9f829b4677e6443fabea
junting <junting0218@gmail.com>
2025-04-09 20:46:29 +0800

    init

---
+++ src/main/java/com/tsmc/cloudnative/attendancesystemapi/AttendanceSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.tsmc.cloudnative.attendancesystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsmc/cloudnative/attendancesystemapi/AttendanceSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsmc.cloudnative.attendancesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
